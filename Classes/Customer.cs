a2466cbb67f212ed8a6ff995b05abf07e52b7747
Denidalan <125769544+Denidalan@users.noreply.github.com>
2024-02-18 01:24:04 +0300

    Sınıflar

---
+++ Classes/Customer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+	public class Customer
+	{
+		//Property
+        public int Id { get; set; }
+
+		//Field
+		private string _firstName;
+		public string FirstName
+		{
+			get
+			{
+				return "Mr. "+ _firstName;
+
+			}
+			set
+			{
+				_firstName = value;
+
+			}
+		}
+		public string LastName { get; set; }
+
+		public string Address { get; set; }
+    }
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Classes
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			CustomerManager customerManager = new CustomerManager();
+			customerManager.Add();
+			customerManager.Update();
+
+			ProdactManager prodactManager = new ProdactManager();
+			prodactManager.Update();
+			prodactManager.Add();
+
+			Customer customer = new Customer();
+			customer.Address = "İstanbul";
+			customer.FirstName = "Test";
+			customer.LastName = "Test";
+			customer.Id = 1;
+
+			Customer customer1 = new Customer
+			{
+				Id = 2, FirstName = "Test1", LastName = "Test1", Address="Bursa"
+
+			};
+
+            Console.WriteLine(customer1.FirstName);
+        }
+	}
+
+	
+
+	
+}
