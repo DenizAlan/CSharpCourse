cf17bbf04f366f5cbd76160a03d574fb8abf3b9e
Denidalan <125769544+Denidalan@users.noreply.github.com>
2024-02-17 23:32:59 +0300

    Diziler

---
+++ Arrays/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Arrays
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] students = new string[3];
+			students[0] = "Deniz";
+			students[1] = "Özlem";
+			students[2] = "Hacer";
+
+			string[] students2 = new[] { "Çagla", "Ayşe", "Fatma" };
+			string[] students3 = { "Aslan", "Kaplan", "Kedi" };
+
+
+            foreach (var student in students)
+            {
+                Console.WriteLine(student);
+            }
+			Console.WriteLine();
+
+			//Çok boyutlu diziler
+			string[,] regions = new string[5, 3]  //5 satır 3 kolon demek
+			{
+				{ "İst","Bursa ","İzmit " } ,
+				{ "Ankara ","Kayseri","Konya" } ,
+				{ "Antalya","Adana","Mersin" } ,
+				{ "Rize","Trabzon","Samsun" } ,
+				{ "İzmir","Muğla","Manisa" } ,
+			
+			};
+
+			// satırlar 0. dimension suturlar 1. dimension
+			//her satır için 3 tanede sutun gezmem lazım
+			for (int i = 0; i <= regions.GetUpperBound(0); i++)
+			{
+				for (int j = 0; j <= regions.GetUpperBound(1); j++)
+				{
+					Console.WriteLine(regions[i,j]);
+                }
+                Console.WriteLine("************");
+            }
+
+			Console.ReadLine();
+        }
+	}
+}
