a6350172673b570fe1cb422fdb31ccc1ffa372aa
Denidalan <125769544+Denidalan@users.noreply.github.com>
2024-02-18 23:06:48 +0300

    Interfaces2

---
+++ İnterfaces2/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace İnterfaces2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//1. Yol
+			IPerson customer = new Customer();  //Referans tip adres tuttuugu için böyle newlenebilir
+			ProjectManager projectManager = new ProjectManager();
+			projectManager.Add(customer);
+            //2.Yol
+            projectManager.Add(new Employes());
+
+            Console.WriteLine();
+        }
+	}
+
+	interface IPerson
+	{
+		void Add();
+		void Update();
+	}
+
+	class Customer : IPerson
+	{
+		public void Add()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Update()
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	class Employes : IPerson
+	{
+		public void Add()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Update()
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	class ProjectManager
+	{
+		//interfaceden implante edilen diğer classlara ulaşır
+		public void Add(IPerson person)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
